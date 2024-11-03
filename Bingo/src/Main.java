b5d255680488c4fc884dd779b2b4058494cfe9c5
AinhoaGarcíaSanz <ainhoagarciasanz02@gmail.com>
2024-11-03 15:30:38 +0100

    Tablero_Bingo

--- Bingo/src/Main.java
+++ Bingo/src/Main.java
@@ -12,11 +12,8 @@ public class Main {
             System.out.println(" ");
         }
     }
-    public static void main(String[] args) {
-       tablero= new int[MAX_FILA_TABLERO][MAX_COLUMNA_TABLERO];
-       Random aleatorio = new Random();
-
-       //Rellenar tablero dentro de los rangos de cada columna
+    private static void rellenarTablero() {
+        Random aleatorio= new Random();
         for (int i = 0; i < MAX_FILA_TABLERO; i++) {
             for (int j = 0; j < MAX_COLUMNA_TABLERO; j++) {
                 int min= 10+(j*10); //Rango mínimo de la columna, si j es 0 (1ºa columna), su min será 10
@@ -27,35 +24,34 @@ public class Main {
                 //Quedaría así tablero[i][j] = aleatorio.nextInt(10) + 10, al generar el 0 le sumará los 10.
             }
         }
-        //Imprimimos tablero sin ordenar
-        imprimirTablero();
-        //Ordenamos las casillas de menor a mayor en cada columna
-        for (int columna = 0; columna < MAX_COLUMNA_TABLERO; columna++) { //Recorre las columnas del tablero
-            for (int i = 0; i < MAX_FILA_TABLERO-1; i++) { //Recorre cada fila, es -1 porque va a tener que comparar 2 filas, no habrá una 3ra para comparar
-                for (int j = 0; j < MAX_FILA_TABLERO -i -1; j++) { //Compara dos filas dentro de la columna, es -i -1 porque conforme i aumenta, nº de filas a comparar disminuye.
-                    if (tablero[j][columna] > tablero[j+1][columna]){ //Si el valor en la fila j es mayor que el de la fila siguiente (j + 1), deben intercambiarse.
-                        int aux= tablero[j][columna]; //Guardamos el valor de la fila actual en la variable auxiliar
-                        tablero[j][columna]= tablero[j+1][columna];//Se almacena en esa fila el valor de la fila siguiente
-                        tablero[j+1][columna]= aux; //Asignamos el valor de aux de antes al valor que acabamos de recibir, es decir, pasa a la columna siguiente a la inicial
-                        //Ejemplo: Columna 0
-                        // [18]
-                        // [12]
-                        // [14]
-                        // j=0 (PRIMERA COMPARACIÓN) aux=18   tablero[0][0]= 12  tablero[1][0]= aux= 18
-                        // [12]
-                        // [18]
-                        // [14]
-                        //j=1 (SEGUNDA COMPARACIÓN) aux=18   tablero[0][0]= 14  tablero[1][0]= aux= 18
-                        // [12]
-                        // [14]
-                        // [18]
+    }
+    private static void ordenarTablero() {
+        int cont=0;
+        do {
+            for (int i = 0; i < MAX_FILA_TABLERO -1; i++) { //Límites, va hasta filas -2 (por ir comparando)
+                for (int j = 0; j < MAX_COLUMNA_TABLERO; j++) { //Va hasta columnas -1
+                    if (tablero[i][j] > tablero[i + 1][j]) {
+                        int aux;
+                        aux = tablero[i][j];
+                        tablero[i][j] = tablero[i + 1][j];
+                        tablero[i + 1][j] = aux;
                     }
                 }
             }
-        }
+            cont++;
+        }while (cont < MAX_FILA_TABLERO); //Se repetirá el nº de filas que haya
+    }
+    public static void main(String[] args) {
+        tablero= new int[MAX_FILA_TABLERO][MAX_COLUMNA_TABLERO];
+        //Rellenar tablero dentro de los rangos de cada columna
+        rellenarTablero();
+        //Imprimimos tablero sin ordenar
+        imprimirTablero();
+        //Ordenamos las casillas de menor a mayor en cada columna
+        ordenarTablero();
         //Imprimir tablero ordenado
         System.out.println(" ");
-        System.out.println("Tablero ordenado por columnas:");
+        System.out.println("Tablero ordenado por columnas: ");
         imprimirTablero();
     }
 }
