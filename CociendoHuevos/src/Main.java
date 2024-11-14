84a645434f012c3cbef1a69b3547ecf1a6339811
AinhoaGarcíaSanz <ainhoagarciasanz02@gmail.com>
2024-11-14 12:19:24 +0100

    Acepta el reto 009

---
+++ CociendoHuevos/src/Main.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+    private static final Scanner lector= new Scanner(System.in);
+
+    public static void main(String[] args) {
+        System.out.println("¿Cuántos casos de prueba quieres?");
+        int casosPrueba= lector.nextInt();
+
+        for (int i = 0; i < casosPrueba; i++) {
+            System.out.println("Dime los huevos que quieres cocer");
+            int huevos= lector.nextInt();
+            System.out.println("Dime la capacidad en huevos de la olla");
+
+            int capacidadHuevosOlla= lector.nextInt();
+            if ((1<huevos&& huevos<=10000) && (1<capacidadHuevosOlla  && capacidadHuevosOlla<=1000)){
+                int numCiclos= (huevos+capacidadHuevosOlla-1)/capacidadHuevosOlla;
+                int minutosCoccion= numCiclos*10;
+                System.out.println(minutosCoccion);
+
+            }else if (huevos<=0 || capacidadHuevosOlla<=0){
+                System.out.println("Valor no válido");
+                break;
+            }
+
+        }
+    }
+}
